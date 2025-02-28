ea06544e4d7aa050447aff39560c620709b5efe4
NguyenQuangHuy888 <huynqtd00446@fpt.edu.vn>
2025-02-28 10:15:41 +0700

    2 methods 6 tests

---
+++ Lab8.Tests/ArrayTests.cs
@@ -0,0 +1,58 @@
+﻿using Lab8B2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.Tests
+{
+    [TestClass]
+    public class ArrayTest
+    {
+        [TestMethod]
+        public void FindMax()
+        {
+            int[] array = { 3, 5, 1, 6, 10 };
+            int expected = 10;
+            int Max = Calculator.FindMax(array);
+            Assert.AreEqual(expected, Max);
+        }
+        [TestMethod]
+        public void FindMin()
+        {
+            int[] array = { 3, 5, 1, 6, 10 };
+            int expected = 1;
+            int Min = Calculator.FindMin(array);
+            Assert.AreEqual(expected, Min);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMinLength0()
+        {
+            int[] array = { };
+            Calculator.FindMin(array);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMinNull()
+        {
+            int[] array = null;
+            Calculator.FindMin(array);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMaxLength0()
+        {
+            int[] array = { };
+            Calculator.FindMax(array);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMaxNull()
+        {
+            int[] array = null;
+            Calculator.FindMax(array);
+        }
+    }
+}
---
+++ Lab8B2/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8B2
+{
+    public class Calculator
+    {
+        public static int FindMax(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentException("Array can't be null or empty");
+            }
+            int max = array[0];
+            foreach (int num in array)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+        public static int FindMin(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentException("Array can't be null or empty");
+            }
+            int min = array[0];
+            foreach (int num in array)
+            {
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            return min;
+        }
+    }
+}
